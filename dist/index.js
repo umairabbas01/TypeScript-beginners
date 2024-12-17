9a6d7273a2980a962bea5eb1fcd60cf839180d85
Umair Abbas <umairabbasyash@gmail.com>
2024-12-17 17:17:09 +0500

    DONE

---
+++ dist/index.js
@@ -0,0 +1,12 @@
+"use strict";
+let a2 = 1; // Declare variable with type
+a2 = 10;
+function add3(a, b) {
+    return a + b;
+}
+console.log(add3(1, 2)); // Logs 
+// function greeting() {
+//   return console.log("Hello, world");
+// }
+// console.log("Hi");
+// console.log("Hello");

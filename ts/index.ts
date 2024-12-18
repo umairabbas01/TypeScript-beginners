f8b351b3de0e8c9046c42b890e9de8a7bc6f690b
Umair Abbas <umairabbasyash@gmail.com>
2024-12-18 17:13:07 +0500

    update

--- ts/index.ts
+++ ts/index.ts
@@ -78,6 +78,62 @@ let user;
 
 let response: [number,string]=[200, 'sucess']
 response.push(0);   // drawback in ts
-console.log('response', response);
+// console.log('response', response);
 
 // enum
+// PascalCase => first world are capital
+
+enum Color {
+    // APP_NAME = 'my_app_name',
+    Red= 'red',
+    Green = 'green',
+    Blue = 'blue'
+}
+// console.log(Color.APP_NAME);
+console.log(Color.Blue);
+let color: Color = Color.Green
+console.log(color);
+
+
+enum Size {
+    small,
+    Medium,
+    Large
+}
+
+let size: Size = Size.Medium;
+// console.log('size: ', size);
+
+
+// unknown type
+let notSure: unknown = 'a';
+
+if(typeof(notSure)==='number'){
+    notSure.toFixed(2);
+}else if(typeof(notSure) === 'string'){
+    notSure.length;
+}
+
+
+// never type   infinite
+function infiniteLoop(): never {
+    while(true){
+        // do something endlessly
+        console.log(1);
+        
+    }
+}
+// infiniteLoop()
+
+function throwError(message: string):never {
+    throw new Error(message);
+}
+// throwError('Something went wrong')
+
+
+// void type
+function logMessage(message: string): void {
+    console.log(message);
+    // return null;
+if(message) return 0;
+}
